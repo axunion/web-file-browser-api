470a7f54997b7e527c458db9e24cabad5e75354a
axunion <120321953+axunion@users.noreply.github.com>
2024-10-08 23:49:24 +0900

    Relocate error handling for file uploads

--- src/save_uploaded_file.php
+++ src/save_uploaded_file.php
@@ -1,30 +1,7 @@
 <?php
 
-const MAX_FILESIZE = 16 * 1024 * 1024 * 1024;
-
 function save_uploaded_file(array $uploaded_file, string $destination_path, string $filename): string
 {
-    if (!isset($uploaded_file['error']) || is_array($uploaded_file['error'])) {
-        throw new RuntimeException('Invalid parameters.');
-    }
-
-    switch ($uploaded_file['error']) {
-        case UPLOAD_ERR_OK:
-            break;
-        case UPLOAD_ERR_NO_FILE:
-            throw new RuntimeException('No file sent.');
-        case UPLOAD_ERR_INI_SIZE:
-            throw new RuntimeException('Exceeded filesize limit.');
-        case UPLOAD_ERR_FORM_SIZE:
-            throw new RuntimeException('Exceeded filesize limit.');
-        default:
-            throw new RuntimeException('Unknown errors.');
-    }
-
-    if ($uploaded_file['size'] > MAX_FILESIZE) {
-        throw new RuntimeException('Exceeded filesize limit.');
-    }
-
     $filename = preg_replace("/[^a-zA-Z0-9_.-]/", "", $filename);
 
     if (strlen($filename) === 0) {

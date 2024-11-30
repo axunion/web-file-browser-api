633e2276aa9229876c66f0f66aaff719f236a3c5
axunion <120321953+axunion@users.noreply.github.com>
2024-11-30 17:26:54 +0900

    Correct endpoint import paths

--- public/web-file-browser-api/upload/index.php
+++ public/web-file-browser-api/upload/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../src/save_uploaded_file.php';
+require_once __DIR__ . '/../../../src/save_uploaded_file.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Allow: POST');
